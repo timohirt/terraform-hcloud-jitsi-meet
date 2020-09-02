952ae87f9d5f1e2fd8912bf2264eddb4ab2e9538
Timo Hirt <timo.hirt@gmail.com>
2020-09-02 21:15:20 +0200

    Add ipv4 address output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,9 @@ output "jitsi_server_id" {
     value = hcloud_server.jitsi_server.id
     description = "The id of the server which runs jitsi."
 }
+
+output "jitsi_server_ipv4_address" {
+    value = hcloud_server.jitsi_server.ipv4_address
+    description = "The ipv4 address assigned to the Jitsi server." 
+}
+
